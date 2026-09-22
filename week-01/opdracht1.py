b71bbd87d9b94399ca80af41c124a3d78cb3172a
seidutariq-droid <seidutariq@gmail.com>
2026-09-22 11:37:34 +0200

    opdracht 1 afgemaakt

--- week-01/opdracht1.py
+++ week-01/opdracht1.py
@@ -1,34 +1,50 @@
 # Oefening 1
 # Print de volgende zin "Hello World"
 
-print()
+print("hello world") 
 
 
 # Oefening 2
 # Verander de waarde van de onderstaande variabelen.
 # Print deze daarna 1 voor 1 uit
 
-naam = ""
-leeftijd = 0
-woonstad = ""
-
+naam = "rick"
+leeftijd = 17
+woonstad = "utrecht"
+print("rick")
+print("17")
+print("utrecht")
 
 # Oefening 3
 # Gebruik nu bovenstaande variabelen om zinnen te bouwen
 # Bijvoorbeeld print("Hallo mijn naam is ", naam) of print(f"Mijn naam is {naam}")
-
+print("hallo mijn naam is",rick)
 
 
 # Oefening 4
 # Maak variabelen aan voor je favoriete game, hoe veel uur je deze hebt gespeeld en welk cijfer je dit spel zou geven
 # Print deze daarna in zinnen uit, bijvoorbeeld "Mijn favoriete game is Minecraft" "Ik heb deze game 150 uur gespeeld", "Ik geef deze game een 8.5"
-
+print("mijn favouriteGame game" + blackmyth wukong) 
+print("ik heb deze game al + str11 uur gespeeld" + "uur gespeeld")
+print("ik geef deze game een + str 7")
 
 
 # Oefening 5
 # Maak twee variabelen aan, number1 en number2
 # Bereken daarna de som (+), het verschil (-) en het product (*) uit van deze nummers.
 # Print daarna de uitkomsten uit
+(number1 = 5)
+(number2 = 7)
+
+print(number1)
+print(number2)
+(som = number1 + number2)
+(verschil = number1 - number2)
+(product = number1 * number2)
+print("het verschil is" + str(verschil))
+print("de som is: " + str(som))
+print("het product  is: " + str(product))
+
 
 
 
@@ -36,19 +52,69 @@ woonstad = ""
 # Maak een simpel game character met minimaal de volgende variabelen: name, health, level, damage
 # Print deze vervolgens uit
 # Zorg er daarna voor dat je character 20 damage neemt, print nu de nieuwe waarde van zijn health uit
+name = "Tariq"
+health = 100
+level = 1
+damage = 10
 
+print("Naam: " + name)
+print("Health: " + str(health))
+print("Level: " + str(level))
+print("Damage: " + str(damage))
+print("")
+print(Tariq + " neemt 20 schade!")
+health = health - 20
+print("Nieuwe health: " + str(health))
 
 
 # Oefening 7
 # Ga verder met je character van de vorige oefening. Voeg nu een nieuw variabel "weapon" toe.
 # Geef het wapen een naam, verhoog de damage van je character en verhoog het level met 1
 # Print daarna de nieuwe waardes uit 
+name = "Knight"
+health = 100
+level = 1
+damage = 10
 
+print("Naam: " + name)
+print("Health: " + str(health))
+print("Level: " + str(level))
+print("Damage: " + str(damage))
+print("")
+print(name + " neemt 20 schade!")
+health = health - 20
+print("Nieuwe health: " + str(health))
 
 
 # Oefening 8
-# Maak een programma dat een profiel van een gamer laat zien
+# aak een programma dat een profiel van een gamer laat zien
 # Maak minimaal de volgende variabelen: name, age, favouriteGame, hoursPlayed, level, score
 # Print al deze informatie netjes uit
 # Verhoog daarna de score van het profiel met 250 en print de nieuwe waarde
-# Bonus! Voeg zelf 3 nieuwe variabelen toe
+# Bonus! Voeg zelf 3 nieuwe variabelen toe doe hetzelfde met deze
+
+name = "ShadowX"
+age = 16
+favouriteGame = "Minecraft"
+hoursPlayed = 250
+level = 34
+score = 5000
+rank = "Gold"
+team = "Cyber Wolves"
+comfort = "Montezuma"
+
+print("=== GAMER PROFIEL ===")
+print("Naam: " + name)
+print("Leeftijd: " + str(age))
+print("Favoriete game: " + favouriteGame)
+print("Uren gespeeld: " + str(hoursPlayed))
+print("Level: " + str(level))
+print("Score: " + str(score))
+print("Rank: " + rank)
+print("Team: " + team)
+print("Comfort: " + comfort)
+
+print("")
+print(name + " verdient 250 bonuspunten!")
+score = score + 250
+print("Nieuwe score: " + str(score))
